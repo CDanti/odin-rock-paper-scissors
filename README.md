0f4b2d8bc4c4bd2b35acd0b8318775f16e4614eb
CDanti <chrisdanti2001@gmail.com>
2022-05-31 14:39:54 -0400

    working rps game in webpage console

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rock-paper-scissors
+
+This is my attempt at The Odin Project Rock Paper Scissors project. At the first attempt, we are to use no CSS or HTML to make the page look presentable. The Rock Paper Scissors (RPS) game is all done through the console. This is what my first iteration does, which was completed May 31st 2022 at 2:40pm est.
---
+++ script.js
@@ -0,0 +1,54 @@
+
+function computerPlay(){
+    let computerOptions = ["rock", "paper", "scissors"]
+    let index = Math.floor(Math.random() * (computerOptions.length))
+    let computerOutput = computerOptions[index]
+    return computerOutput
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    console.log("Player Selection: " + playerSelection)
+    console.log("Computer Selection: " + computerSelection)
+    let result
+    if (playerSelection == computerSelection){
+        return result = "tie"
+    }
+    else if (
+        playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper"
+    ){
+        return result = "win"
+    }
+    else{
+        return result = "lost"
+    }
+}
+
+function game(){
+    let wins = 0
+    let losses = 0
+    let ties = 0
+    for (let i = 0; i < 5; i++) {
+        let playerSelection=(prompt("Please enter Rock, Paper, or Scissors: "));
+        let computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection)
+        if (result == "win"){
+            wins ++
+            console.log("You won! :) The computer said " + computerSelection + " and you said " + playerSelection)
+        }
+        else if (result == "lost"){
+            losses ++
+            console.log("You lost! :(  The computer said " + computerSelection + " and you said " + playerSelection)
+        }
+        else if (result == "tie"){
+            ties ++
+            console.log("Tied! :| Both you and the Computer said " + computerSelection)
+        }
+    }
+    return ("Wins: "+wins+"\nLosses: "+losses+"\nTies: "+ties)
+}
+ 
+let result = 0
+console.log(game());
