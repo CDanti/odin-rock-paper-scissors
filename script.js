19d09eadb1370be7983cc1cb809b2704c994fc3a
Cdanti <chrisdanti2001@gmail.com>
2022-06-20 17:13:49 -0400

    resultTextDiv not found for some reason? help!

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 window.onload=function(){
+
+    //find the results box and creare the text div
+    const resultBox = document.querySelector('#resultBox');
+    const resultTextDiv = document.createElement('div');
+    resultBox.appendChild(resultTextDiv);
+    
     const rockBtn = document.querySelector('#rock');
     const paperBtn = document.querySelector('#paper');
     const scissorsBtn = document.querySelector('#scissors');
+    const resetBtn = document.querySelector('#reset');
     
-    rockBtn.addEventListener('click', () => {playRound("rock", computerPlay())})
-    paperBtn.addEventListener('click', () => {playRound("paper", computerPlay())})
+    rockBtn.addEventListener('click', () => { playRound("rock", computerPlay());})
+    paperBtn.addEventListener('click', () => {playRound("paper", computerPlay());})
     scissorsBtn.addEventListener('click', () => {playRound("scissors", computerPlay())})
-   
+    resetBtn.addEventListener('click', () => {clearResult()})    
 }
-
 
 function computerPlay(){
     let computerOptions = ["rock", "paper", "scissors"]
@@ -19,64 +25,64 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection) {
 
-    const resultBox = document.querySelector('#resultBox');
-    const resultTextDiv = document.createElement('div');
-    resultBox.appendChild(resultTextDiv);
 
-    const resultTextTie = document.createElement('p');
-    resultTextTie.textContent = "You tied! :|";
-
-    const resultTextWin = document.createElement('p');
-    resultTextWin.textContent = "You won! :)";
-
-    const resultTextLost = document.createElement('p');
-    resultTextLost.textContent = "You lost! :(";
+    //create the text paragraph that will get put inside the text div
+    const resultText = document.createElement('p');
 
     playerSelection = playerSelection.toLowerCase()
     console.log("Player Selection: " + playerSelection)
     console.log("Computer Selection: " + computerSelection)
+
+    //RPS logic
     let result
-    if (playerSelection == computerSelection){
-        resultTextDiv.appendChild(resultTextTie);
-        return console.log(result = "tie")
+    if (playerSelection == computerSelection){ //if player ties
+        resultText.textContent = "You tied! :|"; //set result text
+        resultTextDiv.appendChild(resultText) //add result text to result div
+        console.log(result = "tie")
     }
-    else if (
+    else if ( //if player wins
         playerSelection == "rock" && computerSelection == "scissors" ||
         playerSelection == "paper" && computerSelection == "rock" ||
-        playerSelection == "scissors" && computerSelection == "paper"
-    ){
-        resultTextDiv.appendChild(resultTextWin);
-        return console.log(result = "win")
+        playerSelection == "scissors" && computerSelection == "paper"){
+        resultText.textContent = "You won! :)"; //set result text
+        resultTextDiv.appendChild(resultText); //add result text to result div
+        console.log(result = "win")
     }
-    else{
-        resultTextDiv.appendChild(resultTextLost);
-        return console.log(result = "lost")
+    else{ //if player loses
+        resultText.textContent = "You lost! :("; //set result text
+        resultTextDiv.appendChild(resultText); //add result text to result div
+        console.log(result = "lost")
     }
 }
 
-function game(playerSelection){
-    let wins = 0
-    let losses = 0
-    let ties = 0
-    // for (let i = 0; i < 5; i++) {
-        // let playerSelection=(prompt("Please enter Rock, Paper, or Scissors: "));
-        let computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection)
-        if (result == "win"){
-            wins ++
-            console.log("You won! :) The computer said " + computerSelection + " and you said " + playerSelection)
-        }
-        else if (result == "lost"){
-            losses ++
-            console.log("You lost! :(  The computer said " + computerSelection + " and you said " + playerSelection)
-        }
-        else if (result == "tie"){
-            ties ++
-            console.log("Tied! :| Both you and the Computer said " + computerSelection)
-        }
-    // }
-    return ("Wins: "+wins+"\nLosses: "+losses+"\nTies: "+ties)
+function clearResult(){
+    console.log("REMOOOOOOOOOVE")
+    resultTextDiv.removeChild(resultText);
 }
+
+// function game(playerSelection){
+//     let wins = 0
+//     let losses = 0
+//     let ties = 0
+//     // for (let i = 0; i < 5; i++) {
+//         // let playerSelection=(prompt("Please enter Rock, Paper, or Scissors: "));
+//         let computerSelection = computerPlay();
+//         result = playRound(playerSelection, computerSelection)
+//         if (result == "win"){
+//             wins ++
+//             console.log("You won! :) The computer said " + computerSelection + " and you said " + playerSelection)
+//         }
+//         else if (result == "lost"){
+//             losses ++
+//             console.log("You lost! :(  The computer said " + computerSelection + " and you said " + playerSelection)
+//         }
+//         else if (result == "tie"){
+//             ties ++
+//             console.log("Tied! :| Both you and the Computer said " + computerSelection)
+//         }
+//     // }
+//     return ("Wins: "+wins+"\nLosses: "+losses+"\nTies: "+ties)
+// }
  
 let result = 0
 // console.log(game());
